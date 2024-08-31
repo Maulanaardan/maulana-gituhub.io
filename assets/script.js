8eef0c74fcbd5760a10225c515def1956c96ed8f
Maulanaardan <141204129+Maulanaardan@users.noreply.github.com>
2024-08-31 13:54:38 +0700

    Add files via upload
    commit 1

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+        strings: ['Developer', 'Designer', 'Freelancer'],
+        typeSpeed: 100,
+        loop: true,
+        shufle: true,
+        backSpeed: 50
+    })
